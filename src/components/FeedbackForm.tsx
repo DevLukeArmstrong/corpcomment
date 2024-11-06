98f13d56f6e88f02153ae36e4086eac6ffa62bc2
Luke Armstrong <Ltarmstrong96@gmail.com>
2024-11-06 15:24:30 -0800

    Add feedback form and list components with error handling and loading state

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/contants";
+
 export default function FeedbackForm() {
-  return <div>FeedbackForm</div>;
+  const [formText, setFormText] = useState("");
+  const charCount = MAX_CHARACTERS - formText.length;
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    if (newText.length > MAX_CHARACTERS) return;
+    setFormText(newText);
+  };
+
+  return (
+    <form className="form">
+      <textarea
+        value={formText}
+        onChange={handleChange}
+        id="feedback-textarea"
+        placeholder=""
+        spellCheck={false}
+      />
+
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
+
+      <div>
+        <p className="u-italic">{charCount}</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
 }
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,3 +1,64 @@
+import { useEffect, useState } from "react";
+import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
+
 export default function FeedbackList() {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+      );
+
+      if (!response.ok) {
+        setErrorMessage("Error fetching data");
+        return;
+      }
+
+      const data = await response.json();
+      setFeedbackItems(data.feedbacks);
+    };
+
+    fetchFeedbackItems();
+    setIsLoading(false);
+
+    // Historical code to use fetch
+
+    // setIsLoading(true);
+    // fetch(
+    //   "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+    // )
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw new Error();
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(data => {
+    //     setFeedbackItems(data.feedbacks);
+    //     setIsLoading(false);
+    //   })
+    //   .catch(() => {
+    //     // network error, not 2xx reponse, or JSON parsing error
+    //     setIsLoading(false);
+    //     setErrorMessage("Error fetching data");
+    //   });
+  }, []);
+
+  return (
+    <ol className="feedback-list">
+      {isloading ? <Spinner /> : null}
+
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+
+      {feedbackItems.map(feedbackItem => (
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
+      ))}
+    </ol>
+  );
 }
