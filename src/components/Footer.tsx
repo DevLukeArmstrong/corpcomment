4b17cd223a25a14449e0b544f249f749d8d9a131
Luke Armstrong <Ltarmstrong96@gmail.com>
2024-11-06 09:15:43 -0800

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://lukearmstrong.dev">
+            Armstrong Industries.
+          </a>
+        </p>
+        <p>
+          <span className="u-bold u-italic">2024</span>
+        </p>
+      </small>
+    </footer>
+  );
+}
